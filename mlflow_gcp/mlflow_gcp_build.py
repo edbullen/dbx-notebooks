cbe3e2abee7cd9e2c27ee60277e97ffc84078832
edbullen <edbullen>
2023-01-10 18:03:37 +0000

    Changes to mlflow training loop to format plots

--- mlflow_gcp/mlflow_gcp_build.py
+++ mlflow_gcp/mlflow_gcp_build.py
@@ -4,7 +4,7 @@
 # MAGIC 
 # MAGIC Demonstration using a publicly available retail banking data-set to train a *Credit Card Fraud Detection* model.  
 # MAGIC 
-# MAGIC Make the model available for multiple access paths via Pyspark, SQL and MLflow and integrate with GCP:
+# MAGIC Make the model available for multiple access paths via Pyspark and SQL in the data-layer via Databricks and also via a Rest API using MLflow as well as demonstration of integrating the model and model results with GCP (Google cloud):
 # MAGIC + Batch scoring - query in SQL via a Spark UDF  
 # MAGIC + Serve out of the MLflow Rest API directly for real-time scoring
 # MAGIC + Write results out to GCP BigTable.
@@ -33,17 +33,11 @@
 # MAGIC   c) store artefacts with each model version for explainablility and governance.     
 # MAGIC 2. **Run**: Test using the model via different interfaces (notebook 2)  
 # MAGIC   a) Batch-score results via Databricks SQL with UDF  
-# MAGIC   b) Batch Score and write results to GCP Hbase (BigTable)  
-# MAGIC   c) Access via Vertex AI (GCP) endpoint. 
+# MAGIC   b) Get predictions from the Databricks MLflow Rest API  
+# MAGIC   c) Batch Score and write results to GCP Hbase (BigTable)  
+# MAGIC   d) Access via Vertex AI (GCP) endpoint. 
 # MAGIC 
 # MAGIC The model is built as a Scikit-learn Random Forest model, managed in the MLflow framework and deployed in Databricks for using directly against the data-lake as well as into Vertex.AI for serving.
-# MAGIC 
-# MAGIC Service Account setup:
-# MAGIC 
-# MAGIC + Privileges for Vertex: `Storage Admin`, `Vertex AI Administrator`   
-# MAGIC + Privileges for HBASE: `BigTable User`  
-# MAGIC 
-# MAGIC Blog refs: https://www.databricks.com/blog/2022/08/12/mlops-on-databricks-with-vertex-ai-on-google-cloud.html 
 
 # COMMAND ----------
 
@@ -59,6 +53,14 @@
 # MAGIC + DBR LTS 10.4 Spark 3.2.1 
 # MAGIC + DBR LTS 11.3 Spark 3.3.0 
 # MAGIC   - Vertex AI Real-time Scoring issue : 'google.protobuf.pyext._message.RepeatedCompositeCo' object has no attribute 'WhichOneof'
+# MAGIC 
+# MAGIC **Config notes**   
+# MAGIC Service Account setup:
+# MAGIC 
+# MAGIC + Privileges for Vertex: `Storage Admin`, `Vertex AI Administrator`   
+# MAGIC + Privileges for HBASE: `BigTable User`  
+# MAGIC 
+# MAGIC Blog refs: https://www.databricks.com/blog/2022/08/12/mlops-on-databricks-with-vertex-ai-on-google-cloud.html 
 
 # COMMAND ----------
 
@@ -76,10 +78,6 @@
 # MAGIC + Feature 'Time' contains the seconds elapsed between each transaction and the first transaction in the dataset. 
 # MAGIC + The feature 'Amount' is the transaction amount. 
 # MAGIC + Feature **'Class'** is the response variable and it takes value 1 in case of fraud and 0 otherwise.
-
-# COMMAND ----------
-
-display(dbutils.fs.ls('/databricks-datasets//credit-card-fraud/'))
 
 # COMMAND ----------
 
@@ -121,6 +119,7 @@ pandas_df = (df.withColumn("pca", vector_to_array("pcaVector"))).select(["time",
 
 # COMMAND ----------
 
+# DBTITLE 1,View First 5 rows of Pandas Dataframe
 pandas_df.loc[0:5]
 
 # COMMAND ----------
@@ -136,7 +135,7 @@ pandas_df.value_counts(subset=['label'])
 
 # COMMAND ----------
 
-# DBTITLE 1,Visualise Data
+# DBTITLE 1,Visualise Data with Pairplots
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -168,7 +167,10 @@ sns.pairplot( sample_df
 
 # DBTITLE 1,Divide dataframe into X features and y Labels
 # in this example we missed off the first two features - time and amount-range. This is just for simplicity - probably they are good features
+
+# X is the features - cols 3 to the end of the df
 X = pandas_df.iloc[:,3:]
+# y is the labels - col 2 of the df
 y = pandas_df.iloc[:,2]
 
 # COMMAND ----------
@@ -201,21 +203,17 @@ y_train.head()
 
 # COMMAND ----------
 
-
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC ## Train a Model - Vanilla
 # MAGIC 
 # MAGIC ### Basic Concepts in Machine Learning
 # MAGIC 
 # MAGIC In general we either train a model based on past known events - this is known as **Supervised Learning**.  Supervised Learning can fall into categories of either 
-# MAGIC + *Classification* (is this a picture of a cat or a dog, is this a fraud or not a fraud)  
-# MAGIC + *Regression* (how much will this stock be worth in the future, what is the predicited weather temperature range)
+# MAGIC + *Classification* (i.e "is this a picture of a cat or a dog?", "is this a fraud or not a fraud?")  
+# MAGIC + *Regression* (i.e. "how much will this stock be worth in the future?", "what is the predicited weather temperature range?")
 # MAGIC 
 # MAGIC It is also possible to create machine learning algorithms that don't require training on previous known outcomes.  This is known as **Unsupervised Learning**.  Most supervised learning is achieved by various
-# MAGIC + *Clustering* algorithms (identify different related groups of data, which data-points can we consider anomolies)
+# MAGIC + *Clustering* algorithms (usually identify different related groups of data or which data-points can we consider anomolies)
 # MAGIC 
 # MAGIC #### Supervised Learning process
 # MAGIC Labeled data is needed to train a model to generate label predictions.  
@@ -239,8 +237,8 @@ y_train.head()
 
 # MAGIC %md
 # MAGIC   
-# MAGIC Optionally, skip forward to Train a Model - MLFlow
-# MAGIC 
+# MAGIC Optionally, skip forward to Train a Model - MLFlow.  This section covers a basic intro to training an ML model with Random Forest.  
+# MAGIC Two types of Random Forest model:  
 # MAGIC `RandomForestRegressor` - this gives a percentage likelihood of 1 or 0.  It also takes longer to train.  
 # MAGIC `RandomForestClassifier` - this gives a 1 / 0 indicator of Fraud / Not-Fraud
 
@@ -280,7 +278,7 @@ plt.show()
 # MAGIC Here is a zoomed in view of the top part of one of the trees:
 # MAGIC 
 # MAGIC 
-# MAGIC <img src="https://drive.google.com/uc?export=view&id=1-Qchz3_BDdwFldRoQX1Tj2CwvX70XK4u" alt="drawing" width="1200"/>
+# MAGIC <img src="https://drive.google.com/uc?export=view&id=1-Qchz3_BDdwFldRoQX1Tj2CwvX70XK4u" alt="drawing" width="800"/>
 
 # COMMAND ----------
 
@@ -373,7 +371,7 @@ plt.show()
 # MAGIC ## Train a Model - MLFlow
 # MAGIC 
 # MAGIC + *Experiments* - track all our machine-learning development runs and associated artefects
-# MAGIC + *Feature-Store* - manage the data we use to train the model; retain this for model reproducibility and team collaboration
+# MAGIC + *Feature-Store* - manage the data we use to train the model; retain this for model reproducibility and team collaboration (*this has been missed out of this demo*)
 # MAGIC + *Models* - stored in the Databricks environment with namespace mapped to release cycle deployment
 
 # COMMAND ----------
@@ -398,17 +396,21 @@ import mlflow
 # COMMAND ----------
 
 # DBTITLE 1,MLFlow model train - takes 2 minutes
+# import Scikit-Learn open source ML libraries
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, precision_score, recall_score
+
+# MLflow library for recording the model signature
 from mlflow.models.signature import infer_signature
 
+# open source python libs
 import seaborn as sns
 import numpy as np
 
 import time
 
 # set a maximum number of branches (speed up training time)
-max_depth = 10
+max_depth = [10]
 
 # assess the impact of different number of trees
 estimators = [10, 20, 50]
@@ -416,68 +418,72 @@ estimators = [10, 20, 50]
 # timestamp to append to the run-name for this set of runs
 run_timestamp = int(time.time())
 
-for n_estimators in estimators:
-  with mlflow.start_run(run_name=f'cc_fraud_{n_estimators}_{run_timestamp}') as run:
-    # Auto-logging to log metrics, parameters, and models without explicit log statements. silent = suppress event logs and warnings
-    mlflow.sklearn.autolog(silent=True)
+for depth in max_depth:
+    for n_estimators in estimators:
+      with mlflow.start_run(run_name=f'cc_fraud_{n_estimators}_{run_timestamp}') as run:
+        # Auto-logging to log metrics, parameters, and models without explicit log statements. silent = suppress event logs and warnings
+        #mlflow.sklearn.autolog(silent=True)
 
-    # Set the hyperparameters for the model - i.e. estimators is the number of trees to use
-    model_rf = RandomForestClassifier(n_estimators = n_estimators
-                                     , random_state = 42
-                                     , max_depth = max_depth
-                                     , n_jobs = 16)
-    # Train the model on training data
-    model_rf.fit(X_train, y_train);
+        # Set the hyperparameters for the model - i.e. estimators is the number of trees to use
+        model_rf = RandomForestClassifier(n_estimators = n_estimators
+                                         , random_state = 42
+                                         , max_depth = depth
+                                         , n_jobs = 16)
+        # Train the model on training data
+        model_rf.fit(X_train, y_train);
 
-    # accuracy metrics
-    y_pred = model_rf.predict(X_test).astype(int)
+        # accuracy metrics
+        y_pred = model_rf.predict(X_test).astype(int)
 
-    accuracy = accuracy_score(y_test, y_pred)*100
-    prec = precision_score(y_test, y_pred)*100
-    rec = recall_score(y_test, y_pred)*100
-    f1 = f1_score(y_test, y_pred)*100
+        accuracy = accuracy_score(y_test, y_pred)*100
+        prec = precision_score(y_test, y_pred)*100
+        rec = recall_score(y_test, y_pred)*100
+        f1 = f1_score(y_test, y_pred)*100
 
-    # set a tag so we can find this group of experiment runs
-    mlflow.set_tag("project", "cc_fraud")
-    
-    # infer the model signature before we log it
-    signature = infer_signature(X_train, model_rf.predict(X_train))
+        # set a tag so we can find this group of experiment runs
+        mlflow.set_tag("project", "cc_fraud")
 
-    #Log model called "cc_fraud" in the MLflow repository with parameters, and metrics
-    mlflow.sklearn.log_model(model_rf
-                             , "cc_fraud"
-                             , input_example=X_train.loc[1:1]
-                             , serialization_format = 'pickle'
-                             , signature=signature
-                             )
- 
-    mlflow.log_metric('accuracy', accuracy)
-    mlflow.log_metric('precision', prec)
-    mlflow.log_metric('recall', rec)
-    mlflow.log_metric('f1_score', f1)
-    mlflow.log_param('max_depth', max_depth)
-    mlflow.log_param('n_estimators', n_estimators)
-   
-    # example code for manually logging a confusion matrix - auto log does it for us anyway
-    #CM = confusion_matrix(y_test, y_pred)
-    #ax = sns.heatmap(CM, annot=True, cmap='Blues', fmt='.8g')
-    #mlflow.log_figure(plt.gcf(), "test_confusion_matrix.png")
-    
+        # infer the model signature before we log it
+        signature = infer_signature(X_train, model_rf.predict(X_train))
 
-    # store feature importances as an artefact
-    importances = model_rf.feature_importances_.round(3)
-    sorted_indices = np.argsort(importances)[::-1]
-    feat_labels = X_train.columns
-    # setting the dimensions of the plot
-    fig, ax = plt.subplots(figsize=(10, 10))
+        #Log model called "cc_fraud" in the MLflow repository with parameters, and metrics
+        mlflow.sklearn.log_model(model_rf
+                                 , "cc_fraud"
+                                 , input_example=X_train.loc[1:1]
+                                 , serialization_format = 'pickle'
+                                 , signature=signature
+                                 )
 
-    # plot the relative importance of features
-    sns.barplot(x=importances[sorted_indices]
-                , y=feat_labels[sorted_indices]
-                , orient='horizonal')
-    for i in ax.containers:
-        ax.bar_label(i,)
-    mlflow.log_figure(plt.gcf(), "feature_importance.png")    
+        mlflow.log_metric('accuracy', accuracy)
+        mlflow.log_metric('precision', prec)
+        mlflow.log_metric('recall', rec)
+        mlflow.log_metric('f1_score', f1)
+        mlflow.log_param('max_depth', depth)
+        mlflow.log_param('n_estimators', n_estimators)
+
+        # setting the dimensions of the plot
+        fig, ax = plt.subplots(figsize=(6, 6))
+
+        # record a confusion matrix for the model test results
+        CM = confusion_matrix(y_test, y_pred)
+        ax = sns.heatmap(CM, annot=True, cmap='Blues', fmt='.8g')
+        mlflow.log_figure(plt.gcf(), "test_confusion_matrix.png")
+
+        # store feature importances as an artefact
+        importances = model_rf.feature_importances_.round(3)
+        sorted_indices = np.argsort(importances)[::-1]
+        feat_labels = X_train.columns
+
+        # setting the dimensions of the plot
+        fig, ax = plt.subplots(figsize=(8, 8))
+
+        # plot the relative importance of features
+        sns.barplot(x=importances[sorted_indices]
+                    , y=feat_labels[sorted_indices]
+                    , orient='horizonal')
+        for i in ax.containers:
+            ax.bar_label(i,)
+        mlflow.log_figure(plt.gcf(), "feature_importance.png")    
 
  
 mlflow.end_run()  
@@ -525,7 +531,12 @@ print("model version "+model_registered.version+" as been registered as producti
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Deploy Model Vertex AI
+# MAGIC ## Deploy Model to MLflow Rest API
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Deploy Model to a Vertex AI Endpoint
 # MAGIC 
 # MAGIC <img width="100px" src="https://techcrunch.com/wp-content/uploads/2021/05/VertexAI-512-color.png">
 # MAGIC 
@@ -568,9 +579,9 @@ model_uri="models:/cc_fraud/Production"
 
 # MAGIC %md
 # MAGIC 
-# MAGIC ### Deploy to a Vertex AI Endpoint
 # MAGIC 
-# MAGIC #### Cluster Permissions
+# MAGIC 
+# MAGIC **Cluster Permissions**
 # MAGIC The cluster must be configured with a Service Account that has privileges to deploy to the Google API endpoint.  
 # MAGIC 
 # MAGIC EG
