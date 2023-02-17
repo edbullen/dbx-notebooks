cd4a06ae98ec2e02a958b217ee9e94dfc4082588
Ed Bullen <ed.bullen@databricks.com>
2023-02-17 10:40:16 +0000

    simple example of SAS model re-written in Python with MLflow wrapper

--- sas_model/models/SimpleExampleModel.py
+++ sas_model/models/SimpleExampleModel.py
@@ -9,8 +9,8 @@ class SimpleExampleModel(mlflow.pyfunc.PythonModel):
         --------------------
         |  1  |  12  |  f  |
         |  2  |  35  |  m  |
-        |  3  |  75  |  m  |
-        |  4  |  15  |  f  |
+        | ... |  ... |  ...|
+        |  n  |   #  | m/f |
         --------------------
        and returns DF with three cols: ID, young/middle/old age and woman/man output"""
     
@@ -21,21 +21,32 @@ class SimpleExampleModel(mlflow.pyfunc.PythonModel):
     def _model_function(self, model_input :pd.DataFrame):
         """model code is placed here"""
 
-        rows = len(model_input)
+        def age_grp_if(x): 
+            """ inner function - age-class logic """
+            if (x <= 35) :
+                return 'young age'
+            elif (x <= 60):
+                return 'middle age'
+            else:
+                return 'old age'
+            
+        def woman_man_if(x): 
+            """ inner function - man-woman-class logic """
+            if (x == 'f') :
+                return 'woman'
+            elif (x == 'm'):
+                return 'man'
+            else:
+                return 'other'
+    
+        # accumulate output data in lists using Pandas apply-function.  Could re-write to pandas.loc operation for speedup.        
+        age_grp = model_input['age'].apply(age_grp_if)
+        sex = model_input['sex'].apply(woman_man_if)
 
-        dummy_ids = range(1,rows+1)
-        #dummy_age = pd.Series(random.choice([12,20,35,70,89,40]) for _ in range(rows))
-        #dummy_sex = pd.Series(random.choice(["f", "m"]) for _ in range(rows))
-        dummy_age_group = pd.Series(["middle_age"]).repeat(rows)
-        dummy_sex = pd.Series(["m"]).repeat(rows)
+        return pd.DataFrame({'ID':model_input['ID'], 'age_grp':age_grp, 'sex':sex})    
 
-        dummy_data = pd.DataFrame({'ID':dummy_ids, 'age': dummy_age_group, 'sex': dummy_sex })
-        
-        model_output = dummy_data
-        
-        return model_output
 
     def predict(self, context, model_input :pd.DataFrame):
         """create a predict() method for MLflow to use - calls the custom fn to get results"""
         
-        return model_input.apply(self._model_function)
+        return self._model_function(model_input)
--- sas_model/tests/test_SimpleExampleModel.py
+++ sas_model/tests/test_SimpleExampleModel.py
@@ -1,21 +1,42 @@
 import pandas as pd
+import pytest
 
 from models.SimpleExampleModel import SimpleExampleModel
 
 
-def test_model():
-    """ test function behaves as expected:
-    values less than x0 -> y0, values greater-equal x0 -> y = mx + c   (eqn of straight line)
-        m is the gradient, c is the intercept = y0 - (gradient * x0)
-    """
-    # instance of the model
+# set up some test data as a fixture to pass in to tests
+@pytest.fixture
+def test_data():
+
+    # create some test data in a data-frame, id 1..4
+    test_ids = range(1,5)
+    test_age = pd.Series([12,20,70,40])
+    test_sex = pd.Series(["m","f","m","f"])
+    
+    df = pd.DataFrame({'ID':test_ids, 'age':test_age, 'sex': test_sex})
+
+    return df
+
+
+# get the model results to check
+@pytest.fixture
+def model_output(test_data):
+     # instance of the model
     model = SimpleExampleModel()
     
-    # test data in a Pandas series -2 ... 12
-    data = pd.DataFrame([range(-2, 13)])
-
     # apply the model's _model_function to get output in a Pandas series
-    model_output = data.apply(model._model_function, axis=1)
+    return model._model_function(test_data)
 
-    assert int(2) == 2
+
+def test_age_grp(model_output):
+    """ test age_grp logic behaves as expected"""
     
+    # check we get the expexted output for age_grp
+    assert all([a == b for a, b in zip(model_output['age_grp'], ['young age', 'young age', 'old age', 'middle age'])])
+   
+
+def test_sex(model_output):
+    """ test man/woman logic behaves as expected"""
+
+    # check we get the expexted output for sex
+    assert all([a == b for a, b in zip(model_output['sex'], ['man', 'woman', 'man', 'woman'])])
